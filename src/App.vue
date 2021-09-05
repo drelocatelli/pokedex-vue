95bed958ba3e327918b8121926d59723005bb33a
Your Name <you@example.com>
2021-09-05 12:25:37 -0300

    ...

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <h1 class="title is-1">Pokedex Search</h1>
+    <div class="columns is-centered">
+      <div class="column is-one-quarter">
+          <input type="text" class="input is-info is-rounded" v-model="busca" placeholder="buscar pokemon pelo nome">
+      </div>
+    </div>
+
+    <div class="columns is-centered">
+      <div class="column is-one-quarter">
+          <div v-for="(poke, index) in resultadoBusca" :key="index">
+            <Pokemon :name="poke.name" :url="poke.url" :num="index"/>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
+
+export default {
+  name: 'App',
+  data(){
+      return{
+            pokemons: [],
+            busca: ''
+      }
+  },
+  created: function(){
+        axios.get("http://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+        .then(res => {
+            this.pokemons = res.data.results
+        });
+  },
+  components:{
+      Pokemon
+  },
+  computed: {
+    resultadoBusca: function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="pokemon">
+        <div class="card">
+            <div class="card-image">
+                <figure>
+                    <img :src="pokemon.sprites.front" width="200px">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">{{name | capitalize}}</p>
+                        <b>Type: </b>
+                        <span>{{pokemon.type | capitalize}}</span>
+                    </div>
+                </div>
+                <hr>
+                <div class="content">
+                    <span v-for="(ability, i) in pokemon.abilities[0]" :key="i">
+                        {{ability.ability.name | capitalize}}<br>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created: function(){
+        axios.get(this.url).then(res => {
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.sprites = {}
+            this.pokemon.id = res.data.id;
+            this.pokemon.sprites.front = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+this.pokemon.id+".png";
+            this.pokemon.abilities = []
+            this.pokemon.abilities.push(res.data.abilities)
+        });
+    },
+    data(){
+        return {
+            pokemon: {
+                type: '',
+                sprites: {
+                    front: '',
+                },
+                abilities: []
+            }
+        }
+    },
+    props: {
+        num : Number,
+        name : String,
+        url : String
+    },
+    filters: {
+        capitalize: function(value){
+            let newName = value[0].toUpperCase() + value.slice(1);
+            return newName;
+        }
+    }
+}
+</script>
+
+<style>
+#pokemon{
+    margin-top:10%;
+}
+</style>
